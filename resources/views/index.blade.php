687a11afd4a62f776815ab0e3328a7d6630bbc28
inFureal <random997@gmail.com>
2020-11-04 01:04:36 +0600

    Add migrate command namespace

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -53,109 +53,121 @@
         @if($custom = config('artisan-gui.commands', []))
             <x-gui-group id="App">
                 @foreach($custom as $command)
-                    <x-gui-command name="{{ $command }}" />
+                    <x-gui-command name="{{ $command }}"/>
                 @endforeach
             </x-gui-group>
         @endif
 
         <x-gui-group id="laravel">
-            <x-gui-command name="clear-compiled" />
-            <x-gui-command name="down" />
-            <x-gui-command name="env" />
-            <x-gui-command name="help" />
-            <x-gui-command name="inspire" />
-            <x-gui-command name="list" />
-            <x-gui-command name="optimize" />
-            <x-gui-command name="optimize:clear" />
-            <x-gui-command name="down" />
-            <x-gui-command name="notifications:table" />
-            <x-gui-command name="package:discover" />
-            <x-gui-command name="schedule:run" />
-            <x-gui-command name="schema:dump" />
-            <x-gui-command name="session:table" />
-            <x-gui-command name="storage:link" />
-            <x-gui-command name="stub:publish" />
+            <x-gui-command name="clear-compiled"/>
+            <x-gui-command name="down"/>
+            <x-gui-command name="env"/>
+            <x-gui-command name="help"/>
+            <x-gui-command name="inspire"/>
+            <x-gui-command name="list"/>
+            <x-gui-command name="optimize"/>
+            <x-gui-command name="optimize:clear"/>
+            <x-gui-command name="down"/>
+            <x-gui-command name="notifications:table"/>
+            <x-gui-command name="package:discover"/>
+            <x-gui-command name="schedule:run"/>
+            <x-gui-command name="schema:dump"/>
+            <x-gui-command name="session:table"/>
+            <x-gui-command name="storage:link"/>
+            <x-gui-command name="stub:publish"/>
         </x-gui-group>
 
         <x-gui-group id="Auth">
-            <x-gui-command name="auth:clear-resets" />
+            <x-gui-command name="auth:clear-resets"/>
         </x-gui-group>
 
         <x-gui-group id="Cache">
-            <x-gui-command name="cache:clear" />
-            <x-gui-command name="cache:forget" />
-            <x-gui-command name="cache:table" />
-            <x-gui-command name="config:clear" />
-            <x-gui-command name="config:cache" />
+            <x-gui-command name="cache:clear"/>
+            <x-gui-command name="cache:forget"/>
+            <x-gui-command name="cache:table"/>
+            <x-gui-command name="config:clear"/>
+            <x-gui-command name="config:cache"/>
         </x-gui-group>
 
         <x-gui-group id="Database">
-            <x-gui-command name="db:seed" />
-            <x-gui-command name="db:wipe" />
+            <x-gui-command name="db:seed"/>
+            <x-gui-command name="db:wipe"/>
         </x-gui-group>
 
         <x-gui-group id="Events">
-            <x-gui-command name="event:cache" />
-            <x-gui-command name="event:clear" />
-            <x-gui-command name="event:generate" />
-            <x-gui-command name="event:list" />
+            <x-gui-command name="event:cache"/>
+            <x-gui-command name="event:clear"/>
+            <x-gui-command name="event:generate"/>
+            <x-gui-command name="event:list"/>
         </x-gui-group>
 
         <x-gui-group id="make">
 
-            <x-gui-command name="make:cast" />
-            <x-gui-command name="make:channel" />
-            <x-gui-command name="make:command" />
-            <x-gui-command name="make:component" />
-            <x-gui-command name="make:controller" />
-            <x-gui-command name="make:event" />
-            <x-gui-command name="make:exception" />
-            <x-gui-command name="make:factory" />
-            <x-gui-command name="make:job" />
-            <x-gui-command name="make:listener" />
-            <x-gui-command name="make:mail" />
-            <x-gui-command name="make:middleware" />
-            <x-gui-command name="make:migration" />
-            <x-gui-command name="make:model" />
-            <x-gui-command name="make:notification" />
-            <x-gui-command name="make:observer" />
-            <x-gui-command name="make:policy" />
-            <x-gui-command name="make:provider" />
-            <x-gui-command name="make:request" />
-            <x-gui-command name="make:resource" />
-            <x-gui-command name="make:rule" />
-            <x-gui-command name="make:seeder" />
-            <x-gui-command name="make:test" />
+            <x-gui-command name="make:cast"/>
+            <x-gui-command name="make:channel"/>
+            <x-gui-command name="make:command"/>
+            <x-gui-command name="make:component"/>
+            <x-gui-command name="make:controller"/>
+            <x-gui-command name="make:event"/>
+            <x-gui-command name="make:exception"/>
+            <x-gui-command name="make:factory"/>
+            <x-gui-command name="make:job"/>
+            <x-gui-command name="make:listener"/>
+            <x-gui-command name="make:mail"/>
+            <x-gui-command name="make:middleware"/>
+            <x-gui-command name="make:migration"/>
+            <x-gui-command name="make:model"/>
+            <x-gui-command name="make:notification"/>
+            <x-gui-command name="make:observer"/>
+            <x-gui-command name="make:policy"/>
+            <x-gui-command name="make:provider"/>
+            <x-gui-command name="make:request"/>
+            <x-gui-command name="make:resource"/>
+            <x-gui-command name="make:rule"/>
+            <x-gui-command name="make:seeder"/>
+            <x-gui-command name="make:test"/>
+
+        </x-gui-group>
+
+        <x-gui-group id="Migrate">
+
+            <x-gui-command name="migrate"/>
+            <x-gui-command name="migrate:fresh"/>
+            <x-gui-command name="migrate:install"/>
+            <x-gui-command name="migrate:refresh"/>
+            <x-gui-command name="migrate:reset"/>
+            <x-gui-command name="migrate:rollback"/>
+            <x-gui-command name="migrate:status"/>
 
         </x-gui-group>
 
         <x-gui-group id="Queue">
 
-            <x-gui-command name="queue:batches-table" />
-            <x-gui-command name="queue:clear" />
-            <x-gui-command name="queue:failed" />
-            <x-gui-command name="queue:failed-table" />
-            <x-gui-command name="queue:flush" />
-            <x-gui-command name="queue:forget" />
-            <x-gui-command name="queue:restart" />
-            <x-gui-command name="queue:retry" />
-            <x-gui-command name="queue:retry-batch" />
-            <x-gui-command name="queue:table" />
+            <x-gui-command name="queue:batches-table"/>
+            <x-gui-command name="queue:clear"/>
+            <x-gui-command name="queue:failed"/>
+            <x-gui-command name="queue:failed-table"/>
+            <x-gui-command name="queue:flush"/>
+            <x-gui-command name="queue:forget"/>
+            <x-gui-command name="queue:restart"/>
+            <x-gui-command name="queue:retry"/>
+            <x-gui-command name="queue:retry-batch"/>
+            <x-gui-command name="queue:table"/>
 
         </x-gui-group>
 
         <x-gui-group id="Route">
 
-            <x-gui-command name="route:cache" />
-            <x-gui-command name="route:clear" />
-            <x-gui-command name="route:list" />
+            <x-gui-command name="route:cache"/>
+            <x-gui-command name="route:clear"/>
+            <x-gui-command name="route:list"/>
 
         </x-gui-group>
 
         <x-gui-group id="View">
 
-            <x-gui-command name="view:cache" />
-            <x-gui-command name="view:clear" />
+            <x-gui-command name="view:cache"/>
+            <x-gui-command name="view:clear"/>
 
         </x-gui-group>
 
