11b374153cc3c23a4e614749ee569b1eb1916359
Tachi <zaveruha007@gmail.com>
2023-12-25 23:27:43 +0200

    - fixed namespace

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
 use Infureal\Http\Controllers\GuiController;
 
 Route::get('/', [GuiController::class, 'index'])->name('gui.index');
